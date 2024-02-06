660f65707076ccae04d483fa9da61b250f979f9e
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-02-06 16:27:30 +0530

    my protfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
+import { ProjectComponent } from './components/project/project.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,AboutComponent,SkillsComponent,ContactComponent,ProjectComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+import { ProjectComponent } from './components/project/project.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+export const routes: Routes = [{ path: 'about', component: AboutComponent },{
+  path:'skills', component:SkillsComponent
+},{
+  path:'contact',component:ContactComponent
+},{
+  path:'project',component:ProjectComponent
+},{
+  path:'',component:HomeComponent
+}];
